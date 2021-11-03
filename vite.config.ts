9bb10eeab1f999794e831bba7bc4d3dcc69913b7
HermitSun <syl1887415157@126.com>
2021-11-03 15:28:41 +0800

    fix: build opts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
     vue(),
     Components({
